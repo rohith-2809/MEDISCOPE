cd031fcf2f517d99c42b71bb2047d7bc2e9c8c5d
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2025-10-21 13:27:15 +0530

    Update signup API endpoint to production URL

--- Client/src/SignUpPage.jsx
+++ Client/src/SignUpPage.jsx
@@ -35,7 +35,7 @@ const SignUpPage = () => {
       setIsLoading(true);
       console.log('Submitting payload:', formData);
 
-      const res = await fetch('http://localhost:4000/signup', {
+      const res = await fetch('https://mediscope-backend.onrender.com/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
