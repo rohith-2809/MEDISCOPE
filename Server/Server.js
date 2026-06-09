91b6ac72d4fea0047e485757fe2796588c485a42
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-06-09 11:17:04 +0530

    feat(fix) : fixed web socketing

--- Server/Server.js
+++ Server/Server.js
@@ -1,10 +1,10 @@
 // =============================
 // MAIN SERVER
 // =============================
-const FormData = require("form-data"); 
-const express = require("express"); 
-const axios = require("axios"); 
-const mongoose = require("mongoose"); 
+const FormData = require("form-data");
+const express = require("express");
+const axios = require("axios");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const { Server } = require("socket.io");
