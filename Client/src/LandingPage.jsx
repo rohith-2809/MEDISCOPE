09847eca7469a36e9f10effd9dd303dbb0002462
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-08-14 06:49:05 +0530

    refactor(ui): update text content for better clarity

--- Client/src/LandingPage.jsx
+++ Client/src/LandingPage.jsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect, useRef } from "react";
 
 
 
+
 import { motion, AnimatePresence, useInView } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import Logo from "./assets/Logo.webp";
